2b793a0a6a3eb7c3465ec255e999672e8a8fe8d0
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-18 18:59:39 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,7 +6,10 @@ def second_challenge
     hero_friends: []
       #your key/value pairs here!
       }, 
-    :capulet => { 
+    :capulet => { patriarch: {},
+    matriarch: {},
+    hero: {},
+    heroine_friends: []
       #your key/value pairs here!
       }}
 
