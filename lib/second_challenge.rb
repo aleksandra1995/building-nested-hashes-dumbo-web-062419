1125127bd87856393737cba6437ab2f266e9d41c
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-18 18:59:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,12 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+    :montague => {patriarch: {},
+    matriarch: {},
+    hero: {},
+    hero_friends: []
       #your key/value pairs here!
       }, 
-    :capulet => {
+    :capulet => { 
       #your key/value pairs here!
       }}
 
